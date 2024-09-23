1f6b5359a26497a4d9978dc32084fc0c7a86f3db
todi-shruti <todishruti2002@gmail.com>
2024-09-23 13:07:33 +0530

    First

--- src/com/deloitte/lab5/ex3/Lab5Ex3.java
+++ src/com/deloitte/lab5/ex3/Lab5Ex3.java
@@ -1,5 +1,36 @@
 package com.deloitte.lab5.ex3;
 
-public class Lab5Ex3 {
+import com.deloitte.lab5.ex3.com.cg.eis.exception.EmployeeException;
+import java.util.Scanner;
 
+public class Lab5Ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter employee salary: ");
+        int salary = scanner.nextInt();
+
+        Employee employee = new Employee(salary);
+
+        try {
+            if (employee.getSalary() < 3000) {
+                throw new EmployeeException("Salary cannot be below 3000");
+            }
+            System.out.println("Employee salary is valid");
+        } catch (EmployeeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
+class Employee {
+    private int salary;
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
---
+++ src/com/deloitte/lab5/ex3/com/cg/eis/exception/EmployeeException.java
@@ -0,0 +1,8 @@
+package com.deloitte.lab5.ex3.com.cg.eis.exception;
+
+
+public class EmployeeException extends Exception {
+    public EmployeeException(String message) {
+        super(message);
+    }
+}
